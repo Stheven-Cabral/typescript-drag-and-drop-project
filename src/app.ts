0ae574bec3f8a19d95c6c8f731c4a99bad678fc8
Stheven Cabral <stheven.cabral@gmail.com>
2020-09-28 17:05:23 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ class ProjectInput {
 
   constructor() {
     // The '!' tells TS we will get an element with that type of id and will not be null.
-    // Using 'as' is called typecasting. We are telling TS what type we are getting from the id.
+    // Using 'as' is called typecasting (changing an entity into another type). We are telling TS what type we are getting from the id.
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
   }
