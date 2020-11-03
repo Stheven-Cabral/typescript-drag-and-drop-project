820cb77c259108d744255275a0e2fa904e71a445
Stheven Cabral <stheven.cabral@gmail.com>
2020-11-02 19:35:43 -0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class ProjectInput {
-  templateElement: HTMLTemplateElement;
+  // The element types you see on the right of the colon are dom element types. In order to use this you have to add "dom" to "lib" of the tsconfig.json file.
+  templateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
   titleInputElement: HTMLInputElement;
@@ -9,11 +10,11 @@ class ProjectInput {
   constructor() {
     // The '!' tells TS we will get an element with that type of id and will not be null.
     // Using 'as' is called typecasting (changing an entity into another type). We are telling TS what type we are getting from the id.
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.templateElement = document.getElementById('project-input')!;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     // importedNode is only exclusive to the constructor.
-    const importedNode = document.importNode(this.templateElement.content, true);
+    const importedNode = document.importNode(this.templateElement, true);
     // 'content' is a property that sits on the HTMLTemplateElement type. References the content of an element.
 
     this.element = importedNode.firstElementChild as HTMLFormElement;
